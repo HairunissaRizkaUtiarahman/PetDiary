49b172b83d3cd2a33f0523483f7efb22fe997c3f
agungisra <agungisra27@gmail.com>
2024-04-04 08:05:04 +0700

    Add Splash Screen & Dashboard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,12 +48,4 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-
-
-
-    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
-    implementation("com.google.firebase:firebase-analytics:21.6.1")
-    implementation("com.google.firebase:firebase-database")
-
-
 }
